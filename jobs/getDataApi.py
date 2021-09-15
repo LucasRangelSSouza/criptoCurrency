e4ce37ca77ad8a5be99d08e6d0ed45832c77836a
lucas.rsoares <lucas.rsoares@totvs.com.br>
2021-09-15 16:09:38 -0300

    Atualizado comentarios no codigo

--- jobs/getDataApi.py
+++ jobs/getDataApi.py
@@ -6,12 +6,24 @@ import apscheduler.schedulers.blocking
 from datetime import datetime
 import time
 import sqlalchemy
+import sys
+
+# Engine de conexão com banco de dados
+database_connection = 'mysql+mysqldb://{user}:{password}@{server}:{port}/{database}'
 
 def getApiData():
+    
+    #obtem data utc
     dt = datetime.utcnow()
+    
+    #Chama API
     url='https://poloniex.com/public?command=returnTicker';
     response = requests.get(url, verify=False)
+    
+    # transforma os dados recebidos em um DataFrame Pandas
     ticker = pd.DataFrame(response.json())
+    
+    #Homogeniza e trata os dados
     moedas = list(ticker.columns.values)
     ticker = ticker.T
     ticker['CRIPTOMOEDA'] = moedas
@@ -20,12 +32,24 @@ def getApiData():
     ticker.drop(collumsToDrop, axis=1, inplace=True)
     ticker.rename(columns={'last': 'PRECO'}, inplace=True)
     ticker['DATETIME'] = dt.strftime('%Y-%m-%d %H:%M:%S')
-    database_connection = sqlalchemy.create_engine('mysql+mysqldb://{user}:{password}@{server}:{port}/{database}'
-                                                   .format(user='admin', password='789123', server='mysql', port='3306',  database='currency'))
+
+    #Salva os dados na tabela TEM do banco de dados
     ticker.to_sql(name='TEMP',con=database_connection,if_exists='append', index=False)
-scheduler = BlockingScheduler()
-scheduler.add_job(getApiData, 'cron', second='*',max_instances=10)
-scheduler.start()
+
+if __name__ == "__main__":
+    args = sys.argv
+    interno = bool((args[1]=='True' and 1 or 0)) #Argumento fora ou no docker
+    databaseParams = json.load(open('databaseParams.json'))
+    database_connection = database_connection.format(user=databaseParams['user'],
+                  password=databaseParams['password'],
+                  server=databaseParams[(interno and 'serverInternal' or 'serverExternal')],
+                  port=databaseParams[(interno and 'portInternal' or 'portExternal')],
+                  database=databaseParams['database'])
+    database_connection = sqlalchemy.create_engine(database_connection)
+
+    scheduler = BlockingScheduler()
+    scheduler.add_job(getApiData, 'cron', second='*',max_instances=10)
+    scheduler.start()
 
 
 
--- jobs/perMinute.py
+++ jobs/perMinute.py
@@ -22,45 +22,57 @@ deleteAfterInsert = False
 database_connection = 'mysql+mysqldb://{user}:{password}@{server}:{port}/{database}'
 
 def perMinuteTicker():
+    #Obtem a data UTC atual (Data de fechamento do candle)
     dtfechamento = datetime.utcnow() - timedelta(seconds=1)
-    print(dtfechamento)
+
+    #Obtem a data de abertura do candle
     dtabertura = dtfechamento - timedelta(minutes=periodicidade)
-    print(dtabertura)
     
+    #Busca os dados na tabela TEMP do banco de dados
     querySelect = "SELECT *  \
             FROM TEMP WHERE DATETIME BETWEEN '%Y-%m-%d %H:%M:%S' AND '#Y-#m-#d #H:#M:#S'" 
     
     querySelect = dtabertura.strftime(querySelect)
     querySelect = querySelect.replace('#','%')
     querySelect = dtfechamento.strftime(querySelect)
-    print(querySelect)
     
+    #Cria um dataframe dos dados obtidos
     dataframe = pd.read_sql(querySelect,con=database_connection)
     dataframe['DATETIME']= pd.to_datetime(dataframe['DATETIME'])
 
+    #Seleciona os valores de abertura dos candles de todas as crypto
     abertura = dataframe[(dataframe['DATETIME'] == dtabertura.strftime('%Y-%m-%d %H:%M:%S'))]
     abertura = abertura[['CRIPTOMOEDA', 'PRECO']]
     abertura.rename(columns={'PRECO': 'ABERTURA'}, inplace=True)
 
+    #Seleciona os valores de fechamento dos candles de todas as crypto
     fechamento = dataframe[(dataframe['DATETIME'] == (dtfechamento - timedelta(seconds=1)).strftime('%Y-%m-%d %H:%M:%S'))]
     fechamento = fechamento[['CRIPTOMOEDA', 'PRECO']]
     fechamento.rename(columns={'PRECO': 'FECHAMENTO'}, inplace=True)
 
+    #Seleciona os valores de maximo dos candles de todas as crypto
     high = dataframe[['CRIPTOMOEDA', 'PRECO']]
     high = high.groupby("CRIPTOMOEDA").max()
     high.rename(columns={'PRECO': 'MAXIMO'}, inplace=True)
 
+    #Seleciona os valores de minimo dos candles de todas as crypto
     low = dataframe[['CRIPTOMOEDA', 'PRECO']]
     low = low.groupby("CRIPTOMOEDA").min()
     low.rename(columns={'PRECO': 'MINIMO'}, inplace=True)
     
+    # Realiza o merge dos dataframes de abertura, fechamento, maximo e minimo
     ticker = pd.merge(pd.merge(pd.merge(abertura,fechamento,on='CRIPTOMOEDA'),high,on='CRIPTOMOEDA'),low,on='CRIPTOMOEDA')
+    
+    #Inclui a data de fechamento do candle e a periodicidade
     ticker['DATETIME'] = dtfechamento.strftime('%Y-%m-%d %H:%M:%S')
     ticker['PERIODICIDADE'] = periodicidade
+
+    #Insere os candles na base dados
     ticker = ticker.reset_index()
     ticker = ticker[['CRIPTOMOEDA', 'ABERTURA','FECHAMENTO','MAXIMO','MINIMO','DATETIME','PERIODICIDADE']]
     ticker.to_sql('CANDYSTICKS',con=database_connection,if_exists='append', index=False)
 
+    #Caso a flag de delete seja True deleta todos os dados com idade superior a 15minutos
     if deleteAfterInsert:
         print('deletando')
         queryDelete = "DELETE \
@@ -73,21 +85,23 @@ def perMinuteTicker():
 
 if __name__ == "__main__":
     args = sys.argv
-    print(sys.argv)
     periodicidade = int(args[1]) #Argumento de periodicidade
     interno = bool((args[2]=='True' and 1 or 0)) #Argumento fora ou no docker
     deleteAfterInsert = bool((args[3]=='True' and 1 or 0)) #Argumento se deleta dados analisados
     
+    #Busca paramentros de conexão com o banco de dados
     databaseParams = json.load(open('databaseParams.json'))
-    print(databaseParams)
     database_connection = database_connection.format(user=databaseParams['user'],
                   password=databaseParams['password'],
                   server=databaseParams[(interno and 'serverInternal' or 'serverExternal')],
                   port=databaseParams[(interno and 'portInternal' or 'portExternal')],
                   database=databaseParams['database'])
+    
+    #Cria a engine de conexão com o banco
     database_connection = sqlalchemy.create_engine(database_connection)
     minuteString = str(np.arange(0, 59, periodicidade)).replace(" ",",").replace(",,",",")[2:-1]
 
+    #Cria o agendamento do job a cada um segundo, para execultar a função perMinuteTicker
     scheduler = BlockingScheduler()
     scheduler.add_job(perMinuteTicker, 'cron', minute=minuteString,second='1',max_instances=10)
     scheduler.start()
